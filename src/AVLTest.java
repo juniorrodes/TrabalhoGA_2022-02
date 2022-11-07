96643890709098d3afb424c51091021ef4b1bad7
José Luís <juniorrodes@gmail.com>
2022-11-07 19:57:52 -0300

    :art: Code refactor done

--- src/AVLTest.java
+++ src/AVLTest.java
@@ -1,14 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class AVLTest {
 
-	public static void main(String[] args) {
-		    Node t = new Node(5);
+	private static Scanner reader = new Scanner(System.in);
 
-		    t.insert(5);
-		    t.insert(1);
-		    t.insert(3);
-		    t.insert(9);
-		    t.insert(4);
-		    t.insert(2);
-//		    t.mostraArvore();
+	public static void main(String[] args) {
+//		Node t = new Node(readInt("Digite o valor do primeiro nó da árvore:"));
+//
+//		int value = readInt("Digite valor do nó:");
+//
+//		while (value != 0) {
+//			t.insert(value);
+//			value = readInt("Digite valor do nó:");
+//		}
+		Node t = new Node(8);
+
+		t.insert(10);
+		t.insert(49);
+		t.insert(5);
+		t.insert(6);
+		t.insert(7);
+		t.insert(15);
+		t.insert(24);
+		t.insert(13);
+		t.insert(35);
+		t.insert(46);
+
+
+		List<Integer> list = t.inOrder();
+		for (Integer i : list) {
+			System.out.printf("%d ", i);
 		}
+
+		System.out.println();
+
+		list = t.preOrder();
+		for (Integer i : list) {
+			System.out.printf("%d ", i);
+		}
+
+		System.out.println();
+
+		list = t.postOrder();
+		for (Integer i : list) {
+			System.out.printf("%d ", i);
+		}
+
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+		t.delete(8);
+
+		list = t.inOrder();
+		for (Integer i : list) {
+			System.out.printf("%d ", i);
+		}
+
+		System.out.println();
+
+		list = t.preOrder();
+		for (Integer i : list) {
+			System.out.printf("%d ", i);
+		}
+
+		System.out.println();
+
+		list = t.postOrder();
+		for (Integer i : list) {
+			System.out.printf("%d ", i);
+		}
+
+//		t.mostraArvore();
 	}
+
+	private static int readInt(String msg) {
+		System.out.println(msg);
+		return reader.nextInt();
+	}
+}
--- src/Node.java
+++ src/Node.java
@@ -5,9 +5,12 @@
 //e as melhores coisas
 //n�o tem l�gica
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
 	private int balanceFactor;
-	private final int key;
+	private int key;
 	private int height;
 
 	private Node leftChild;
@@ -21,14 +24,63 @@ public class Node {
 		this.balanceFactor = 0;
 	}
 
+	private Node(int key, int height, int balanceFactor) {
+		this.leftChild = null;
+		this.rightChild = null;
+		this.key = key;
+		this.height = height;
+		this.balanceFactor = balanceFactor;
+	}
+
 	public Node find(int value) {
-		if (value > this.key) {
+		if (value > this.key && this.rightChild != null) {
 			return this.rightChild.find(value);
-		} else if (value < this.key) {
+		}
+		if (value < this.key && this.leftChild != null) {
 			return this.leftChild.find(value);
-		} else {
+		}
+		if (value == this.key) {
 			return this;
 		}
+
+		return null;
+	}
+
+	private Node findMax() {
+		return this.rightChild == null ? this : this.rightChild.findMax();
+	}
+
+	public Node delete(int value) {
+		if (find(value) == null) {
+			return null;
+		}
+
+		Node newNode = null;
+
+		if (value < this.key) {
+			this.leftChild = this.leftChild.delete(value);
+			newNode = this;
+		} else if (value > this.key) {
+			this.rightChild = this.rightChild.delete(value);
+			newNode = this;
+		} else {
+			if (this.leftChild == null) {
+				newNode = this.rightChild;
+			} else if (this.rightChild == null) {
+				newNode = this.leftChild;
+			} else {
+				Node max = this.leftChild.findMax();
+
+				this.key = max.key;
+				this.leftChild = this.leftChild.delete(max.key);
+
+				newNode = this;
+			}
+		}
+
+		this.updateBalance();
+		this.updateTreeHeight();
+		return newNode;
 	}
 
 	public void insert(int value) {
@@ -52,18 +104,133 @@ public class Node {
 		}
 	}
 
-	public void updateBalance() {
-		int leftTreeHeight = this.leftChild.height;
-		int rightTreeHeight = this.rightChild.height;
-
-		this.balanceFactor = leftTreeHeight - rightTreeHeight;
-	}
-
 	public void updateTreeHeight() {
-		int leftTreeHeight = this.leftChild.height;
-		int rightTreeHeight = this.rightChild.height;
+		int leftTreeHeight = this.leftChild != null ? this.leftChild.height : 0;
+		int rightTreeHeight = this.rightChild != null ? this.rightChild.height : 0;
 
 		this.height = Math.max(leftTreeHeight, rightTreeHeight) + 1;
+	}
+
+	public void updateBalance() {
+		int leftTreeHeight = this.leftChild != null ? this.leftChild.height : 0;
+		int rightTreeHeight = this.rightChild != null ? this.rightChild.height : 0;
+
+		this.balanceFactor = leftTreeHeight - rightTreeHeight;
+
+		if (this.balanceFactor <= -2 || this.balanceFactor >= 2) {
+			Node newNode = this.balanceTree();
+
+			this.updateNodeReference(newNode);
+		}
+	}
+
+	private void updateNodeReference(Node newNode) {
+		Node copy = new Node(this.key, this.height, this.balanceFactor);
+		copy.leftChild = this.leftChild;
+		copy.rightChild = this.rightChild;
+
+		this.key = newNode.key;
+		this.height = newNode.height;
+		this.balanceFactor = newNode.balanceFactor;
+
+		if (newNode.leftChild == this) {
+			this.leftChild = copy;
+			this.rightChild = newNode.rightChild;
+		} else if (newNode.rightChild == this) {
+			this.rightChild = copy;
+			this.leftChild = newNode.leftChild;
+		}
+	}
+
+	private Node balanceTree() {
+		if (this.balanceFactor == 2) {
+			if (this.leftChild.balanceFactor == -1) {
+				this.leftChild= this.leftChild.rotateLeft();
+			}
+
+			return this.rotateRight();
+		} else if (this.balanceFactor == -2) {
+			if(this.rightChild.balanceFactor == 1) {
+				this.rightChild = this.rightChild.rotateRight();
+			}
+
+			return this.rotateLeft();
+		}
+
+		return this;
+	}
+
+	private Node rotateLeft() {
+		Node newRoot;
+
+		newRoot = this.rightChild;
+		this.rightChild = newRoot.leftChild;
+		newRoot.leftChild = this;
+
+		updateAttributes(newRoot);
+
+		return newRoot;
+	}
+
+	private Node rotateRight() {
+		Node newRoot;
+
+		newRoot = this.leftChild;
+		this.leftChild = newRoot.rightChild;
+		newRoot.rightChild = this;
+
+		updateAttributes(newRoot);
+
+		return newRoot;
+	}
+
+	private void updateAttributes(Node newRoot) {
+		this.updateBalance();
+		this.updateTreeHeight();
+		newRoot.updateBalance();
+		newRoot.updateTreeHeight();
+	}
+
+	public List<Integer> inOrder() {
+		List<Integer> list = new ArrayList<Integer>();
+
+		if (this.leftChild != null) {
+			list.addAll(this.leftChild.inOrder());
+		}
+		list.add(this.key);
+		if (this.rightChild != null) {
+			list.addAll(this.rightChild.inOrder());
+		}
+
+		return list;
+	}
+
+	public List<Integer> preOrder() {
+		List<Integer> list = new ArrayList<Integer>();
+
+		list.add(this.key);
+		if (this.leftChild != null) {
+			list.addAll(this.leftChild.preOrder());
+		}
+		if (this.rightChild != null) {
+			list.addAll(this.rightChild.preOrder());
+		}
+
+		return list;
+	}
+
+	public List<Integer> postOrder() {
+		List<Integer> list = new ArrayList<Integer>();
+
+		if (this.leftChild != null) {
+			list.addAll(this.leftChild.postOrder());
+		}
+		if (this.rightChild != null) {
+			list.addAll(this.rightChild.postOrder());
+		}
+		list.add(this.key);
+
+		return list;
 	}
 
 	public int getKey() {
